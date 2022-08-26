20f86eb42475543b164172e25cebd904f94c0703
GonzaBah <go.bahamondes@duocuc.cl>
2022-08-26 00:14:09 -0400

    arreglos menores

--- src/app/pages/pagina4/pagina4.page.ts
+++ src/app/pages/pagina4/pagina4.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pagina4',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Pagina4Page implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
   }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: '¡Éxito!',
+      subHeader: '¡Vehículo registrado!',
+      message: 'Bienvenido conductor UwUber',
+      buttons: ['Continuar'],
+    });
 
+    await alert.present();
+  }
 }
